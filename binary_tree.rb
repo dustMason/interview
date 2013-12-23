7beb629fed8bc3c84674f918b36373b796878ecf
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2013-12-23 13:10:13 -0800

    binary tree size and height

---
+++ binary_tree.rb
@@ -0,0 +1,37 @@
+module BT
+
+  class Node
+    attr_accessor :left, :right, :value
+    def initialize value, left=nil, right=nil
+      @value = value
+      @left = left
+      @right = right
+    end
+
+    def is_leaf?
+      @left.nil? && @right.nil?
+    end
+  end
+
+  def self.size node
+    return 0 if node.nil?
+    return 1 if node.is_leaf?
+    size(node.left) + size(node.right) + 1
+  end
+
+  def self.height node, acc=0
+    return 0 if node.nil?
+    return 1 if node.is_leaf?
+    [height(node.left), height(node.right)].max + 1
+  end
+
+end
+
+bt = BT::Node.new(1, BT::Node.new(2, BT::Node.new(4), BT::Node.new(5)), BT::Node.new(3, BT::Node.new(7), BT::Node.new(8)))
+bt2 = BT::Node.new(1, BT::Node.new(2, BT::Node.new(4, BT::Node.new(2, BT::Node.new(3, BT::Node.new(4))), BT::Node.new(5)), BT::Node.new(3, BT::Node.new(7), BT::Node.new(8))))
+
+puts BT::size(bt) #=> 7
+puts BT::height(bt) #=> 3
+
+puts BT::size(bt2) #=> 10
+puts BT::height(bt2) #=> 6
