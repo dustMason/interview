a8825044a492ccbf29f4602c8d6bd4d1f4e0ba6d
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2013-12-25 12:39:43 -0800

    optimized to remove already-searched movies from the list. still not perfect since it mutates the original hash of movies...

--- bacon_number.rb
+++ bacon_number.rb
@@ -1,21 +1,33 @@
-# a basic breadth-first-search approach. could be heavily optimized by eliminating
-# the already searched movies from the input set.
+# a basic breadth-first-search approach.
 
-def bacon_number starting_actor, movies
-  degrees = 0
-  set = co_stars [starting_actor], movies
-  while degrees < 6
-    break if set.include? "kevin"
-    next_degree_of_separation = co_stars(set, movies)
-    break if set == next_degree_of_separation
-    set = next_degree_of_separation
-    degrees += 1
+require 'set'
+
+class Baconizer
+
+  def initialize movies
+    @movies = movies
   end
-  degrees if set.include? "kevin"
-end
 
-def co_stars actors_to_find, movies
-  movies.select { |movie, actors| !(actors & actors_to_find.to_a).empty? }.values.flatten.uniq
+  def bacon_number starting_actor
+    degrees = 0
+    set = [starting_actor]
+    while degrees < 6
+      next_degree_of_separation = co_stars(*set)
+      break if next_degree_of_separation.include? "kevin" || set == next_degree_of_separation
+      set = next_degree_of_separation
+      degrees += 1
+    end
+    degrees if next_degree_of_separation.include? "kevin"
+  end
+
+  def co_stars *actors_to_find
+    set_of_actors = Set.new
+    @movies.delete_if do |movie, actors| 
+      if !(actors & actors_to_find).empty? then set_of_actors += actors else false end
+    end
+    set_of_actors
+  end
+
 end
 
 movies = {
@@ -28,7 +40,8 @@ movies = {
   movieG: %w{sally ted uffie}
 }
 
-puts bacon_number "ben", movies #=> 4
-puts bacon_number "manny", movies #=> 0
-puts bacon_number "henry", movies #=> 2
-puts bacon_number "sally", movies #=> nil
+%w{ben manny henry sally}.each do |actor|
+  baconizer = Baconizer.new movies.dup
+  puts baconizer.bacon_number actor
+  #=> 4, 0, 2, nil
+end
