14995734f5e8ad598a12a9c105aa6b52e5c4cd1e
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2017-07-12 11:16:29 -0700

    find intersections for multidimensional shapes

--- rectangular_love.rb
+++ rectangular_love.rb
@@ -1,45 +1,37 @@
-class Rect
-  attr_accessor :x, :y, :width, :height
-  def initialize x, y, width=nil, height=nil
-    @x = x
-    @y = y
-    @width = width
-    @height = height
+# abstracted the solution to find intersections between two shapes
+# of any number of dimensions
+
+class Shape
+  attr_accessor :dimensions
+  def initialize *dimensions
+    @dimensions = dimensions.each_slice(2).map do |start, length|
+      { start: start, length: length, end: start + length }
+    end
   end
-  def x2
-    x + width
-  end
-  def y2
-    y + height
-  end
+  
   def to_s
-    [x, y, width, height].join "/"
+    dimensions.map { |d| [d[:start], d[:length]] }.to_s
   end
 end
 
 class Love
   def self.intersection l, r
-    horizontal = false
-    vertical = false
-    if (l.x + l.width >= r.x) && (l.x <= r.x + r.width)
-       horizontal = true
+    does_intersect = r.dimensions.each_with_index.all? do |dim, i|
+      l.dimensions[i][:end] >= dim[:start] && l.dimensions[i][:start] <= dim[:end]
     end
     
-    if (l.y + l.height >= r.y) && (l.y <= r.y + r.height)
-      vertical = true
-    end
-    
-    if horizontal && vertical
-      intersect = Rect.new(
-        [l.x, r.x].max,
-        [l.y, r.y].max,
-      )
-      intersect.width = [l.x2, r.x2].min - intersect.x
-      intersect.height = [l.y2, r.y2].min - intersect.y
-      if intersect.height == 0 || intersect.width == 0
+    if does_intersect
+      starts = l.dimensions.each_with_index.map do |dim, i|
+        [dim[:start], r.dimensions[i][:start]].max
+      end
+      lengths = l.dimensions.each_with_index.map do |dim, i|
+        [dim[:end], r.dimensions[i][:end]].min - starts[i]
+      end
+      
+      if lengths.any? { |l| l == 0 }
         return nil
       else
-        return intersect
+        return Shape.new(*starts.zip(lengths).flatten)
       end
     else
       return nil
@@ -48,15 +40,18 @@ class Love
 end
 
 {
-  [Rect.new(0, 1, 2, 2), Rect.new(3, 1, 1, 2)] => nil,
-  [Rect.new(0, 1, 2, 2), Rect.new(2, 1, 2, 2)] => nil,
-  [Rect.new(0, 1, 2, 4), Rect.new(1, 3, 3, 3)] => Rect.new(1, 3, 1, 2),
-  [Rect.new(0, 1, 4, 2), Rect.new(1, 2, 2, 1)] => Rect.new(1, 2, 2, 1)
+  [Shape.new(0, 2, 1, 4), Shape.new(1, 3, 3, 3)] => Shape.new(1, 1, 3, 2), # A
+  [Shape.new(0, 2, 1, 2), Shape.new(3, 1, 1, 2)] => nil, # B
+  [Shape.new(0, 2, 1, 2), Shape.new(2, 2, 1, 2)] => nil, # C
+  [Shape.new(0, 4, 1, 3), Shape.new(1, 2, 2, 1)] => Shape.new(1, 2, 2, 1), # D
+  [Shape.new(0, 2, 1, 4), Shape.new(1, 3, 2, 3)] => Shape.new(1, 1, 2, 3), # E
+  [Shape.new(0, 2), Shape.new(1, 3)] => Shape.new(1, 1),
+  [Shape.new(0, 2, 1, 4, 1, 4), Shape.new(1, 3, 2, 3, 2, 3)] => Shape.new(1, 1, 2, 3, 2, 3),
 }.each do |input, output|
   result = Love.intersection input[0], input[1]
   if result.to_s == output.to_s
     puts "pass"
   else
-    puts "given #{input}, expected #{output} but got #{result}"
+    puts "given #{input.map(&:to_s)}, expected #{output} but got #{result}"
   end
 end
